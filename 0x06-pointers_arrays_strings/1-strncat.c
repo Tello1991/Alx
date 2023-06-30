589c83a816176371f81ea74ea22e12d38a9eec6f
Tello1991 <miyatellopilot@gmail.com>
2023-06-30 15:37:10 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-		int lengthD, lengthS;
+	int lengthD, lengthS;
 
-			lengthD = 0;
-				lengthS = 0;
+	lengthD = 0;
+	lengthS = 0;
 
-					while (*(dest + lengthD) != '\0')
-								lengthD++;
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
 
-						while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
-								{
-											*(dest + lengthD) = *(src + lengthS);
-													lengthD++;
-															lengthS++;
-																}
-							*(dest + lengthD) = '\0';
-								return (dest);
+	while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
 }
